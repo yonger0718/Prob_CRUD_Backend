c9ddb5f58aec8cf8441487ae3bb45c8ed5fe53d6
yonger0718 <tedg319@gmail.com>
2024-06-13 16:25:53 +0800

    feat: 🎸 Added CORS settings, refactor application.yml
    Setting up CORS setting in Controller, added Spring Security to project,
    refactor yml files

---
+++ src/main/java/tw/com/cathaybk/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package tw.com.cathaybk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll())
+                .cors(Customizer.withDefaults())
+                .csrf((csrf) -> csrf.disable());
+        return http.build();
+    }
+}
